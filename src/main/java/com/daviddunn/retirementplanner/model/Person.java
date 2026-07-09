2bb1a74ff8847bb53e63a8e3280e7e4f9d9021d3
daviddunn <daviddunn99@gmail.com>
2026-07-09 09:22:44 -0400

    Initial Project Setup

---
+++ src/main/java/com/daviddunn/retirementplanner/model/Person.java
@@ -0,0 +1,28 @@
+package com.daviddunn.retirementplanner.model;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    public Person(String firstName, String lastName, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+}
